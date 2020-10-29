26f010dadd2582a9d502f9b1e4ade034c30f9349
Clemens Anzmann <clemens.anzmann@gmail.com>
2020-10-29 12:24:26 +0000

    start with simple functionality

--- demo-app/index.js
+++ demo-app/index.js
@@ -1,4 +1,7 @@
 import GraphSonification from '../lib'
 
-const myNewGS = new GraphSonification({ nodes: [], rels: []})
-myNewGS.sonify()
+const myNewGS = new GraphSonification({ nodes: [{}, {}], rels: [{}, {}, {}]})
+
+document.getElementById('speak').addEventListener('click', () => myNewGS.speak())
+
+document.getElementById('play').addEventListener('click', () => myNewGS.sonify())
